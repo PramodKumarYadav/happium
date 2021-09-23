c81b5dd77fb2c9ef6c8bec1e1c186c3a5bd15023
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-09-23 19:52:12 +0200

    add path desired capabilities and tested okay

---
+++ src/main/java/factories/CapabilitiesFactory.java
@@ -0,0 +1,42 @@
+package factories;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.typesafe.config.Config;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.File;
+import java.io.IOException;
+
+import static factories.TestConfigFactory.getConfig;
+
+// This is what I need.
+// https://www.baeldung.com/jackson-object-mapper-tutorial
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CapabilitiesFactory {
+    // Input fields (used in requests)
+    private String automationName;
+    private String platformName;
+    private String platformVersion;
+    private String deviceName;
+    private String app;
+
+    public static CapabilitiesFactory getCapabilities(String deviceName){
+        Config config = getConfig();
+        String pathDesiredCapabilities = config.getString("pathDesiredCapabilities");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        CapabilitiesFactory capabilitiesFactory = null;
+        try {
+            capabilitiesFactory = objectMapper.readValue(new File(String.format("%s/%s.json", pathDesiredCapabilities, deviceName)), CapabilitiesFactory.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return capabilitiesFactory;
+    }
+}
--- src/test/java/TestSandbox.java
+++ src/test/java/TestSandbox.java
@@ -1,4 +1,5 @@
 import com.typesafe.config.Config;
+import factories.CapabilitiesFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +10,15 @@ public class TestSandbox {
     private static Config config = getConfig();
     private static String hostURI = config.getString("hostURI");
     private static String serverPath = config.getString("serverPath");
+    private static String deviceName = "Pixel_5_API_31";
 
     @Test
     void assertThatConfigFactoryIsWorkingAsDesired() {
         log.info(hostURI);
         log.info(serverPath);
+
+        CapabilitiesFactory capabilitiesFactory = CapabilitiesFactory.getCapabilities(deviceName);
+        log.info(capabilitiesFactory.getAutomationName());
+        log.info(capabilitiesFactory.getDeviceName());
     }
 }
