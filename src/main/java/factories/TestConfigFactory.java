6970408b9fe36be3deb1b19f6a1903652f977547
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-09-23 18:28:49 +0200

    add apache commons

---
+++ src/main/java/factories/TestConfigFactory.java
@@ -0,0 +1,32 @@
+package factories;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+@Slf4j
+public class TestConfigFactory {
+    public static Config getConfig() {
+        Config appConfig = ConfigFactory.load();
+
+        // See if any environment is passed from command line
+        String environment = System.getProperty("env");
+        log.debug("environment passed from command line: {}", environment);
+
+        // If host name is not passed from command line, choose it from application.config
+        if (StringUtils.isEmpty(environment)) {
+            environment = appConfig.getString("env");
+        }
+
+        // Load properties specific for chosen host.
+        log.debug("environment to load config: {}", environment);
+        System.setProperty("chosenEnv", environment);
+        Config hostConfig = ConfigFactory.load(environment);
+
+        // Merge properties from common properties (application.conf) and host specific properties (from host file)
+        Config mergedConfig = hostConfig.withFallback(appConfig);
+        log.info(mergedConfig.getString("hostURI"));
+        return mergedConfig;
+    }
+}
--- src/test/java/TestSandbox.java
+++ src/test/java/TestSandbox.java
@@ -1,11 +1,8 @@
 import com.typesafe.config.Config;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import static factories.TestConfigFactory.getConfig;
-import static org.apache.http.HttpStatus.SC_OK;
 
 @Slf4j
 public class TestSandbox {
