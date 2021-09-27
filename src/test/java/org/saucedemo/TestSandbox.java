299a69b9b12495a1a7d8efbfc460c77b143c6920
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-09-27 18:58:31 +0200

    new way to get capabilities

--- src/test/java/org/saucedemo/TestSandbox.java
+++ src/test/java/org/saucedemo/TestSandbox.java
@@ -1,24 +1,31 @@
+package org.saucedemo;
+
 import com.typesafe.config.Config;
-import org.saucedemo.factories.CapabilitiesFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.saucedemo.factories.capabilities.DeviceCapabilities;
+import org.saucedemo.factories.capabilities.GeneralCapabilities;
 
 import static org.saucedemo.factories.EnvConfigFactory.getConfig;
+import static org.saucedemo.factories.capabilities.DeviceCapabilities.getDeviceCapabilities;
+import static org.saucedemo.factories.capabilities.GeneralCapabilities.getGeneralCapabilities;
 
 @Slf4j
 public class TestSandbox {
     private static Config config = getConfig();
     private static String hostURI = config.getString("hostURI");
     private static String serverPath = config.getString("serverPath");
-    private static String deviceName = "Pixel_5_API_31";
+    private static String deviceName = "Pixel_XL_API_31";
 
     @Test
     void assertThatConfigFactoryIsWorkingAsDesired() {
         log.info(hostURI);
         log.info(serverPath);
 
-        DesiredCapabilities capabilities = new CapabilitiesFactory().getDesiredCapabilities(deviceName);
+        DeviceCapabilities capabilities = getDeviceCapabilities(deviceName);
         log.info(String.valueOf(capabilities));
+
+        GeneralCapabilities generalCapabilities = getGeneralCapabilities();
+        log.info(String.valueOf(generalCapabilities));
     }
 }
