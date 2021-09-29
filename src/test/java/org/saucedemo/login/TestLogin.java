e53c7b01c4d4ffd62883b23e208392481ad0a190
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-09-29 13:53:08 +0200

    refactored test info (#19)

--- src/test/java/org/saucedemo/login/TestLogin.java
+++ src/test/java/org/saucedemo/login/TestLogin.java
@@ -1,32 +1,31 @@
 package org.saucedemo.login;
 
-import com.typesafe.config.Config;
 import io.appium.java_client.AppiumDriver;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.saucedemo.factories.DriverFactory;
-import org.saucedemo.factories.EnvConfigFactory;
 import org.saucedemo.screens.LoginScreen;
 import org.saucedemo.screens.ProductsScreen;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.saucedemo.factories.EnvConfigFactory.getTestSetupDetails;
 
 @Slf4j
 class TestLogin {
-    AppiumDriver driver = DriverFactory.getDriver();
+    AppiumDriver driver;
 
-    LoginScreen loginScreen = new LoginScreen(driver);
-    ProductsScreen productsScreen = new ProductsScreen(driver);
+    LoginScreen loginScreen;
+    ProductsScreen productsScreen;
 
     @BeforeEach
     public void setUp() {
-        Config config = EnvConfigFactory.getConfig();
-        log.info("Running tests on platform: {}", config.getString("platformName").toLowerCase());
-        log.info("Running tests on deviceType: {}", config.getString("deviceType").toLowerCase());
-//        log.info("Running tests on deviceName: {}", config.getString("deviceName").toLowerCase());
-        log.info("Running tests on host: {}", config.getString("host").toLowerCase());
+        getTestSetupDetails();
+
+        driver = DriverFactory.getDriver();
+        loginScreen = new LoginScreen(driver);
+        productsScreen = new ProductsScreen(driver);
     }
 
     @AfterEach
