6bacf046f7d11e7cbd9fca59a00dc04b57f1d0ed
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-09-29 16:50:21 +0200

    tidying up tests

--- src/test/java/org/saucedemo/login/TestLogin.java
+++ src/test/java/org/saucedemo/login/TestLogin.java
@@ -3,6 +3,7 @@ package org.saucedemo.login;
 import io.appium.java_client.AppiumDriver;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.saucedemo.factories.DriverFactory;
@@ -14,18 +15,19 @@ import static org.saucedemo.factories.EnvConfigFactory.getTestSetupDetails;
 
 @Slf4j
 class TestLogin {
-    AppiumDriver driver;
+    AppiumDriver driver = DriverFactory.getDriver();
 
-    LoginScreen loginScreen;
-    ProductsScreen productsScreen;
+    LoginScreen loginScreen = new LoginScreen(driver);
+    ProductsScreen productsScreen = new ProductsScreen(driver);
+
+    @BeforeAll
+    public static void info() {
+        getTestSetupDetails();
+    }
 
     @BeforeEach
     public void setUp() {
-        getTestSetupDetails();
-
-        driver = DriverFactory.getDriver();
-        loginScreen = new LoginScreen(driver);
-        productsScreen = new ProductsScreen(driver);
+        log.info("add any logging related info or device that we pick for each test here.");
     }
 
     @AfterEach
