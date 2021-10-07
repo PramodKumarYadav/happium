edadf3c12a35c76fe32ee4be814beecbb53d9652
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-10-07 23:11:38 +0200

    Fix/add comment (#29)
    * refactored test info
    
    * added remark on synchronize method

--- src/main/java/org/saucedemo/factories/AvailableDevices.java
+++ src/main/java/org/saucedemo/factories/AvailableDevices.java
@@ -2,6 +2,14 @@ package org.saucedemo.factories;
 
 import lombok.extern.slf4j.Slf4j;
 
+/*
+We had a unique problem statement here. Since for parallel execution in appium, you can only run a test on a single device,
+we had this challenge on how to get a unique device name from all the tests that are running in parallel. Each test thread
+was trying to get the device(0). So what we want is, that at the device selection (below method), all the threads
+should run in sequence. Synchronized keyword makes that possible. Below is the article that shows how.
+http://tutorials.jenkov.com/java-concurrency/synchronized.html
+ */
+
 @Slf4j
 public class AvailableDevices {
     private static Integer deviceNumber = 0;
@@ -15,3 +23,4 @@ public class AvailableDevices {
         return deviceName;
     }
 }
+
