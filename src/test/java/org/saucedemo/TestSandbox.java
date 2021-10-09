deee81a4f2597bcfb280c070026e665ac0c2ccf6
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-10-09 18:08:39 +0200

    add tests to parse junit properties (#42)

--- src/test/java/org/saucedemo/TestSandbox.java
+++ src/test/java/org/saucedemo/TestSandbox.java
@@ -5,6 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.saucedemo.factories.devices.AndroidEmulators;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.saucedemo.factories.CapabilitiesFactory.getDesiredCapabilities;
 
 @Slf4j
@@ -19,5 +26,22 @@ public class TestSandbox {
     void getDeviceBasedOnLocation() {
         log.info("deviceName @ position 0: {}", AndroidEmulators.values()[0]);
         log.info("deviceName @ position 1: {}", AndroidEmulators.values()[1]);
+    }
+
+    @Test
+    void getJunitProperties() throws IOException {
+        Properties junitProperties = new Properties();
+        String appConfigPath = "D:\\happium\\src\\main\\resources\\junit-platform.properties";
+        junitProperties.load(new FileInputStream(appConfigPath));
+
+        String parallelMode = junitProperties.getProperty("junit.jupiter.execution.parallel.enabled");
+        String testMode = junitProperties.getProperty("junit.jupiter.execution.parallel.mode.default");
+        String classMode = junitProperties.getProperty("junit.jupiter.execution.parallel.mode.classes.default");
+
+        assertAll("Product Details"
+                , () -> assertEquals("true", parallelMode)
+                , () -> assertEquals("concurrent", testMode)
+                , () -> assertEquals("concurrent", classMode)
+        );
     }
 }
