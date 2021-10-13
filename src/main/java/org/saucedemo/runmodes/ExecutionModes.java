db0198e8aa6b512c6165a384b0e47844f0a97ddd
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-10-13 22:44:32 +0200

    a class to return execution mode based on junit properties file value.

---
+++ src/main/java/org/saucedemo/runmodes/ExecutionModes.java
@@ -0,0 +1,8 @@
+package org.saucedemo.runmodes;
+
+public enum ExecutionModes {
+    CLASS_SERIES_TEST_SERIES,
+    CLASS_SERIES_TEST_PARALLEL,
+    CLASS_PARALLEL_TEST_SERIES,
+    CLASS_PARALLEL_TEST_PARALLEL
+}
