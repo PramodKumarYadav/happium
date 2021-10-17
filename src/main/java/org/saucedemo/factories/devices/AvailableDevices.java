f522c42de3ce61c5f3e1e0724a60f3e87a39ec31
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-10-17 14:39:37 +0200

    add logic to get a free device

--- src/main/java/org/saucedemo/factories/devices/AvailableDevices.java
+++ src/main/java/org/saucedemo/factories/devices/AvailableDevices.java
@@ -1,13 +1,20 @@
 package org.saucedemo.factories.devices;
 
 import com.typesafe.config.Config;
+import io.appium.java_client.AppiumDriver;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.Capabilities;
 import org.saucedemo.factories.EnvConfigFactory;
 import org.saucedemo.runmodes.ExecutionModes;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import static org.saucedemo.runmodes.ExecutionMode.getConfigStrategy;
 import static org.saucedemo.runmodes.ExecutionMode.getExecutionMode;
+import static org.saucedemo.runmodes.ExecutionMode.getFixedThreadCount;
 
 /*
 We had a unique problem statement here. Since for parallel execution in appium, you can only run a test on a single device,
@@ -17,14 +24,16 @@ should run in sequence. Synchronized keyword makes that possible. Below is the a
 http://tutorials.jenkov.com/java-concurrency/synchronized.html
  */
 
+@Data
 @Slf4j
 public class AvailableDevices {
     private static Integer deviceNumber = 0;
     private static Integer emulatorNumber = 5554;
-    private static Integer systemPort = 8200;
+    private static Long systemPort = Long.valueOf(8200);
 
     // Create a map where you can add any device that is already used by a test class
     private static HashMap<String, Device> testClassDevicesMap = new HashMap<>();
+    private static List<Device> freedDevices = new ArrayList<>();
     private static String currentTestClass = "";
 
     /*
@@ -84,15 +93,17 @@ public class AvailableDevices {
         if (! testClassName.equalsIgnoreCase(currentTestClass)) {
             log.info("New testClass tests started for: {}", testClassName);
             currentTestClass = testClassName;
-
-            // Initialize all variables
-            deviceNumber = 0;
-            emulatorNumber = 5554;
-            systemPort = 8200;
+            initializeAllVariables();
         }
 
         log.info("Fetching a unique device for this test!");
         return getAUniqueAndroidEmulator();
+    }
+
+    private static synchronized void initializeAllVariables() {
+        deviceNumber = 0;
+        emulatorNumber = 5554;
+        systemPort = Long.valueOf(8200);
     }
 
     public static synchronized Device getAUniqueAndroidEmulatorPerTestClass(String testClassName) {
@@ -110,23 +121,38 @@ public class AvailableDevices {
 
     // Say when running tests in parallel within a single class.
     public static synchronized Device getAUniqueAndroidEmulator() {
-        Device device = new Device();
+        // If config.strategy = fixed; and you already have fetched devices as many as fixed thread count
+        // then you have to initialize variables to pick up existing devices again and not pick up new devices (real or virtual)
+        log.info("deviceNumber: {}", deviceNumber);
+        if (getConfigStrategy().equalsIgnoreCase("fixed") && deviceNumber == getFixedThreadCount()) {
+            log.info("Thread count reached equal to fixed thread count specified in junit properties file.");
+            log.info("Pick the first free device from list of freedDevices.");
+            Device firstFreeDevice = freedDevices.get(0);
+            log.info("firstFreeDevice: {}", firstFreeDevice);
 
-        log.info("fetching device number: {}", deviceNumber);
-        String deviceName = AndroidEmulators.values()[deviceNumber].toString();
+            // Now this device is no longer free for others. So remove it from the freedDevices list.
+            freedDevices.remove(0);
 
-        // Set all the unique properties for this emulator device (necessary for execution in parallel)
-        device.setDeviceName(deviceName);
-        device.setUdid("emulator-" + emulatorNumber);
-        device.setSystemPort(systemPort);
+            return firstFreeDevice;
+        } else {
+            Device device = new Device();
 
-        // Increment so that next device fetched has unique properties. 
-        deviceNumber++;
-        systemPort++;
-        emulatorNumber = emulatorNumber + 2;
+            log.info("fetching device number: {}", deviceNumber);
+            String deviceName = AndroidEmulators.values()[deviceNumber].toString();
 
-        log.info("Device details: {}", device);
-        return device;
+            // Set all the unique properties for this emulator device (necessary for execution in parallel)
+            device.setDeviceName(deviceName);
+            device.setUdid("emulator-" + emulatorNumber);
+            device.setSystemPort(systemPort);
+
+            // Increment so that next device fetched has unique properties.
+            deviceNumber++;
+            systemPort++;
+            emulatorNumber = emulatorNumber + 2;
+
+            log.info("Device details: {}", device);
+            return device;
+        }
     }
 
     public static synchronized String getIosSimulator() {
@@ -136,6 +162,27 @@ public class AvailableDevices {
 
         deviceNumber++;
         return deviceName;
+    }
+
+    public static synchronized void freeDevice(AppiumDriver driver) {
+        Capabilities capabilities = driver.getCapabilities();
+        log.info("capabilities: {}", capabilities);
+
+        // Get capabilities important for our setup.
+        String avd = (String) capabilities.getCapability("avd");
+        String udid = (String) capabilities.getCapability("udid");
+        // Notice extra space in key systemPort. That's a bug in appium. I raised an issue for it
+        // https://github.com/appium/appium/issues/15979
+        Long systemPort = (Long) capabilities.getCapability("systemPort  ");
+
+        log.info("avd: {}", avd);
+        log.info("udid: {}", udid);
+        log.info("systemPort: {}", systemPort);
+
+        Device freeDevice = new Device(udid, avd, systemPort);
+
+        // Add this device to list of freeDevices
+        freedDevices.add(freeDevice);
     }
 }
 
