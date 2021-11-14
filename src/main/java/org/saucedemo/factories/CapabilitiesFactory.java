d6301a1f83d8b4d5489cf42bf16ec4116ac98eb8
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-11-15 00:06:31 +0100

    updated capabilites to get device from device picker

--- src/main/java/org/saucedemo/factories/CapabilitiesFactory.java
+++ src/main/java/org/saucedemo/factories/CapabilitiesFactory.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.saucedemo.deviceTypes.browserStack.BrowserStackDevice;
+import org.saucedemo.deviceTypes.browserStack.BrowserStackDevicePicker;
 import org.saucedemo.factories.devices.Device;
 
 import java.util.Date;
@@ -93,21 +95,23 @@ public class CapabilitiesFactory {
                 break;
             case "browserstack":
                 // Note that browserstack user and key are fetched from system env variables. Rest all other properties are fetched from config.
-                // Github does not allow dots in secrets. So I have to store these keys as underscores (different than
-                // browserstack wants it to be.
-
-                log.info("buildName: {}", CONFIG.getString("BROWSERSTACK_BUILD_NAME"));
+                // Github does not allow dots in secrets. So I have to store these keys as underscores (i.e. different than browserstack specifies it to be.
                 capabilities.setCapability("browserstack.user", System.getenv("BROWSERSTACK_USER"));
                 capabilities.setCapability("browserstack.key", System.getenv("BROWSERSTACK_KEY"));
-                capabilities.setCapability("app", CONFIG.getString("app"));
+                capabilities.setCapability("app", CONFIG.getString("APP"));
 
-                capabilities.setCapability("project", CONFIG.getString("project"));
-                capabilities.setCapability("build", CONFIG.getString("BROWSERSTACK_BUILD_NAME") + " - " + ADD_DATE_TIME_TO_MAKE_BUILDS_UNIQUE);
+                capabilities.setCapability("project", CONFIG.getString("PROJECT"));
+                String buildName = CONFIG.getString("BROWSERSTACK_BUILD_NAME") + " - " + ADD_DATE_TIME_TO_MAKE_BUILDS_UNIQUE;
+                capabilities.setCapability("build", buildName);
+                log.info("buildName: {}", buildName);
+
                 capabilities.setCapability("name", testClassName);
                 capabilities.setCapability("browserstack.networkLogs", true);
 
-                capabilities.setCapability("device", CONFIG.getString("device"));
-                capabilities.setCapability("os_version", CONFIG.getString("os_version"));
+                BrowserStackDevice device = BrowserStackDevicePicker.getDevice();
+                log.info("browserstack device: {} ; {}", device.getDeviceName(), device.getOsVersion());
+                capabilities.setCapability("device", device.getDeviceName());
+                capabilities.setCapability("os_version", device.getOsVersion());
                 break;
             default:
                 log.info("Inside Host default");
