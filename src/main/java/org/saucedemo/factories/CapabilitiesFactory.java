9fb95a48878f46e693f12fe803e9e7e314da0b68
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-11-14 14:55:33 +0100

    have a final value for date, otherwise, it opens too many build instances - rather than one.

--- src/main/java/org/saucedemo/factories/CapabilitiesFactory.java
+++ src/main/java/org/saucedemo/factories/CapabilitiesFactory.java
@@ -24,6 +24,7 @@ import static org.saucedemo.utils.FileUtils.getFileAsString;
 @Data
 public class CapabilitiesFactory {
     private static final Config CONFIG = EnvConfigFactory.getConfig();
+    private static final Date DATE_TIME_NOW = new Date();
 
     /*
      Capabilities are affected based on below 4 parameters defined in application.conf file.
@@ -101,7 +102,7 @@ public class CapabilitiesFactory {
                 capabilities.setCapability("app", CONFIG.getString("app"));
 
                 capabilities.setCapability("project", CONFIG.getString("project"));
-                capabilities.setCapability("build", CONFIG.getString("BROWSERSTACK_BUILD_NAME") + " - " + new Date());
+                capabilities.setCapability("build", CONFIG.getString("BROWSERSTACK_BUILD_NAME") + DATE_TIME_NOW);
                 capabilities.setCapability("name", testClassName);
                 capabilities.setCapability("browserstack.networkLogs", true);
 
