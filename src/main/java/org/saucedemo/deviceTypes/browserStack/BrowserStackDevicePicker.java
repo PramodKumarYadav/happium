c3150763c3c2af36689cd588bd91e66da7511c5f
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-11-15 18:40:37 +0100

    more readable code.

--- src/main/java/org/saucedemo/deviceTypes/browserStack/BrowserStackDevicePicker.java
+++ src/main/java/org/saucedemo/deviceTypes/browserStack/BrowserStackDevicePicker.java
@@ -16,14 +16,22 @@ public class BrowserStackDevicePicker {
     /** All devices (fixed or random, are to be picked from this list):
      * https://www.browserstack.com/list-of-browsers-and-platforms/app_automate */
     public static synchronized BrowserStackDevice getDevice() {
-        // If a fixed device is specified in browserstack appConfig to be picked; use that.
+        String DEVICE = EnvConfigFactory.getConfig().getString("DEVICE");
+        if (DEVICE.equals("random")) {
+            return getARandomBrowserStackDevice();
+        }else {
+            return getAFixedBrowserStackDevice();
+        }
+    }
+
+    private static BrowserStackDevice getAFixedBrowserStackDevice() {
         String DEVICE = EnvConfigFactory.getConfig().getString("DEVICE");
         String OS_VERSION = EnvConfigFactory.getConfig().getString("OS_VERSION");
-        if (! DEVICE.equals("random")) {
-            BrowserStackDevice device = new BrowserStackDevice(DEVICE, OS_VERSION);
-            return device;
-        }
+        BrowserStackDevice device = new BrowserStackDevice(DEVICE, OS_VERSION);
+        return device;
+    }
 
+    private static BrowserStackDevice getARandomBrowserStackDevice() {
         // If a random model is asked, get a random model else use the modelType that was provided to get a random device
         String MODEL_TYPE = EnvConfigFactory.getConfig().getString("MODEL_TYPE");
         if (MODEL_TYPE.equals("random")) {
@@ -35,10 +43,10 @@ public class BrowserStackDevicePicker {
         String filePath = String.format("%s%s.csv", BROWSERSTACK_DEVICES_PATH, MODEL_TYPE);
         List<BrowserStackDevice> devices = BrowserStackDevicePicker.getDevicesForAModel(filePath);
 
-        return getRandomDevice(devices);
+        return getRandomDeviceFromAParticularModel(devices);
     }
 
-    private static BrowserStackDevice getRandomDevice(List<BrowserStackDevice> devices) {
+    private static BrowserStackDevice getRandomDeviceFromAParticularModel(List<BrowserStackDevice> devices) {
         Random random = new Random();
         Integer randomDevice = random.nextInt(devices.size());
         return devices.get(randomDevice);
