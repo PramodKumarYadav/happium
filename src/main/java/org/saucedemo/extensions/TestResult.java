170e128da476ea35aff5a1cb5710ddd94b5667ae
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-11-21 00:51:02 +0100

    method name makes it clear what is returned.

--- src/main/java/org/saucedemo/extensions/TestResult.java
+++ src/main/java/org/saucedemo/extensions/TestResult.java
@@ -1,31 +1,26 @@
 package org.saucedemo.extensions;
 
 import io.appium.java_client.AppiumDriver;
-import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.JavascriptExecutor;
 
 import static org.saucedemo.extensions.TestExecutionLifecycle.getTestStatus;
 
-@Slf4j
 public class TestResult {
     public static void setTestStatus(AppiumDriver driver, String className) {
         JavascriptExecutor jse = (JavascriptExecutor) driver;
 
         String displayName = className + " : " + TestExecutionLifecycle.getTestName();
-        jse.executeScript("browserstack_executor: {\"action\": \"setSessionName\", \"arguments\": {\"name\":\""+ displayName + "\" }}");
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionName\", \"arguments\": {\"name\":\"" + displayName + "\" }}");
 
         Boolean testStatus = getTestStatus();
-        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \""+ getTestResult(testStatus) +"\", \"reason\": \"" + TestExecutionLifecycle.getReason() + "\"}}");
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"" + getTestResult(testStatus) + "\", \"reason\": \"" + TestExecutionLifecycle.getReason() + "\"}}");
     }
 
-    public static String getTestResult(Boolean testStatus){
-        String testResult;
-        if(testStatus == true){
-            testResult = "failed";
-        }else{
-            testResult = "passed";
+    public static String getTestResult(Boolean testStatus) {
+        if (testStatus == true) {
+            return "failed";
+        } else {
+            return "passed";
         }
-
-        return testResult;
     }
 }
