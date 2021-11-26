f29d30b336b615e4c10c62706e6d62ef11c8a1fa
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-11-26 19:13:20 +0100

    added setTestStatusSauceLabs

--- src/main/java/org/saucedemo/extensions/TestResult.java
+++ src/main/java/org/saucedemo/extensions/TestResult.java
@@ -2,11 +2,35 @@ package org.saucedemo.extensions;
 
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.JavascriptExecutor;
+import org.saucedemo.factories.TestEnvironment;
 
 import static org.saucedemo.extensions.TestExecutionLifecycle.getTestStatus;
+import static org.saucedemo.hosts.localhost.android.EmulatorDevicePicker.freeDevice;
 
 public class TestResult {
-    public static void setTestStatus(AppiumDriver driver, String className) {
+    private TestResult(){
+        throw new IllegalStateException("Utility class");
+    }
+
+    private static final String HOST = TestEnvironment.getConfig().getString("HOST").toLowerCase();
+
+    public static void setTestStatus(AppiumDriver driver, String childTestClassName){
+        switch (HOST) {
+            case "browserstack":
+                setTestStatusBrowserStack(driver, childTestClassName);
+                break;
+            case "saucelabs":
+                setTestStatusSauceLabs(driver);
+                break;
+            case "localhost":
+                freeDevice(driver);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void setTestStatusBrowserStack(AppiumDriver driver, String className) {
         JavascriptExecutor jse = (JavascriptExecutor) driver;
 
         String displayName = className + " : " + TestExecutionLifecycle.getTestName();
@@ -21,6 +45,14 @@ public class TestResult {
             return "failed";
         } else {
             return "passed";
+        }
+    }
+
+    public static void setTestStatusSauceLabs(AppiumDriver driver) {
+        if(getTestResult(getTestStatus()).equalsIgnoreCase("passed")){
+            driver.executeScript("sauce:job-result=passed");
+        } else{
+            driver.executeScript("sauce:job-result=failed");
         }
     }
 }
