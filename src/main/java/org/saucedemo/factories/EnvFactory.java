062c6193d41d4870636f0e470b068a88377cd7db
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-12-02 18:24:58 +0100

    it seems browserstack properties are not loaded.

--- src/main/java/org/saucedemo/factories/EnvFactory.java
+++ src/main/java/org/saucedemo/factories/EnvFactory.java
@@ -2,10 +2,11 @@ package org.saucedemo.factories;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.EnumUtils;
 import org.saucedemo.enums.Host;
 
-//@Slf4j
+@Slf4j
 // For future reference on this topic: https://github.com/lightbend/config
 public class EnvFactory {
     private EnvFactory() {
@@ -18,6 +19,7 @@ public class EnvFactory {
         String hostName = baseConfig.getString("HOST").toUpperCase();
 
         if(EnumUtils.isValidEnum(Host.class, hostName)){
+            log.info("loading properties for host: {}", hostName);
             Config hostConfig = ConfigFactory.load(hostName);
             return hostConfig.withFallback(baseConfig);
         }else{
