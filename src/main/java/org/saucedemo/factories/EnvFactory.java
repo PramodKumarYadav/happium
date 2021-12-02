efdfa817e199e43b16c326e42b687e26ac8e701b
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-12-02 17:25:10 +0100

    now we just check if hostName value exists in enum. Rather than adding a check for each host type.

--- src/main/java/org/saucedemo/factories/EnvFactory.java
+++ src/main/java/org/saucedemo/factories/EnvFactory.java
@@ -2,21 +2,23 @@ package org.saucedemo.factories;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.apache.commons.lang3.EnumUtils;
+import org.saucedemo.enums.Host;
 
 //@Slf4j
 // For future reference on this topic: https://github.com/lightbend/config
-public class TestEnvironment {
+public class EnvFactory {
     public static final Config getConfig() {
         // Load default properties (first from System properties and then from application.conf)
         Config baseConfig = ConfigFactory.load();
-        String HOST = baseConfig.getString("HOST");
+        String hostName = baseConfig.getString("HOST").toUpperCase();
 
-        if(HOST.equalsIgnoreCase("localhost") || HOST.equalsIgnoreCase("browserstack") || HOST.equalsIgnoreCase("saucelabs")){
-            Config HOST_CONFIG = ConfigFactory.load(HOST);
+        if(EnumUtils.isValidEnum(Host.class, hostName)){
+            Config HOST_CONFIG = ConfigFactory.load(hostName);
             Config MERGED_CONFIG = HOST_CONFIG.withFallback(baseConfig);
             return MERGED_CONFIG;
         }else{
-            throw new IllegalStateException(String.format("%s is not a valid host choice. Pick your host from localhost, browserstack or saucelabs", HOST));
+            throw new IllegalStateException(String.format("%s is not a valid host choice. Pick your host from %s", hostName, java.util.Arrays.asList(Host.values())));
         }
     }
 }
