f8ae363e764d63f43aa78c14d67dfd13a94c1cda
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-12-06 18:38:56 +0100

    finally tested okay the deeplink function.

--- src/main/java/org/saucedemo/deeplink/DeepLink.java
+++ src/main/java/org/saucedemo/deeplink/DeepLink.java
@@ -1,11 +1,11 @@
 package org.saucedemo.deeplink;
 
 import com.google.common.collect.ImmutableMap;
-import com.typesafe.config.Config;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.support.PageFactory;
+import org.saucedemo.enums.Platform;
 import org.saucedemo.factories.EnvFactory;
 
 import java.util.ArrayList;
@@ -19,9 +19,9 @@ import java.util.Map;
 
 @Slf4j
 public class DeepLink {
-    AppiumDriver driver;
+    private static final Platform PLATFORM = Platform.valueOf(EnvFactory.getConfig().getString("PLATFORM_NAME"));
 
-    private static final Config CONFIG = EnvFactory.getConfig();
+    AppiumDriver driver;
 
     public DeepLink(AppiumDriver driver) {
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
@@ -33,46 +33,48 @@ public class DeepLink {
         return url;
     }
 
-    // convenience method to make tests more readable and avoid duplication.
-    public void toScreen(String url) {
-        toScreen(url, CONFIG.getString("PACKAGE_NAME"));
-    }
-
-    public void toScreen(String url, String packageName) {
-        log.info("deepLinkURL: {}", url);
-        log.info("packageName: {}", packageName);
-
-        switch (CONFIG.getString("PLATFORM_NAME")) {
-            case "android":
-                Map<String, String> map = new HashMap<>();
-                map.put("url", url);
-                map.put("package", packageName);
-
-                driver.executeScript("mobile:deepLink", map);
+    public void toScreen(String deepLinkURL) {
+        log.info("deepLinkURL: {}", deepLinkURL);
+        switch (PLATFORM) {
+            case android:
+                toAndroidScreen(deepLinkURL);
                 break;
-            case "ios":
-                // Todo: Untested code. Curtsy: https://gist.github.com/biwkf/b0ebb9940e6341ed5e588f148b4381a8
-                /* On a real device it is not possible to simply call driver.get("url"), doing so will open SIRI and query with "url" resulting in a search.
-                Instead we can take advantage of Safari's launch parameters and pass deep link URL as argument.
-                Below is an example: */
-
-                String deepLinkURL = "deeplink://";
-                driver.executeScript("mobile: terminateApp", ImmutableMap.of("bundleId", "com.apple.mobilesafari"));
-
-                List args = new ArrayList();
-                args.add("-u");
-                args.add(deepLinkURL);
-
-                Map<String, Object> params = new HashMap<>();
-                // Open safari
-                params.put("bundleId", "com.apple.mobilesafari");
-                params.put("arguments", args);
-
-                driver.executeScript("mobile: launchApp", params);
-                driver.findElementByAccessibilityId("Open").click();
+            case ios:
+                toIOSScreen(deepLinkURL);
                 break;
             default:
                 break;
         }
+    }
+
+    private void toAndroidScreen(String deepLinkURL) {
+        Map<String, String> map = new HashMap<>();
+        map.put("url", deepLinkURL);
+        map.put("package", EnvFactory.getConfig().getString("PACKAGE_NAME"));
+
+        driver.executeScript("mobile:deepLink", map);
+    }
+
+    /**
+     * Curtsy: https://gist.github.com/biwkf/b0ebb9940e6341ed5e588f148b4381a8
+     * @param deepLinkURL
+     * Note: On a real device it is not possible to simply call driver.get("url"), doing so will open SIRI and query with "url" resulting in a search.
+     * Instead we can take advantage of Safari's launch parameters and pass deep link URL as argument. Below is an example:
+     */
+    private void toIOSScreen(String deepLinkURL) {
+        driver.executeScript("mobile: terminateApp", ImmutableMap.of("bundleId", "com.apple.mobilesafari"));
+
+        // Create arguments to open in safari
+        List args = new ArrayList();
+        args.add("-U");
+        args.add(deepLinkURL);
+
+        // Open safari
+        Map<String, Object> params = new HashMap<>();
+        params.put("bundleId", "com.apple.mobilesafari");
+        params.put("arguments", args);
+
+        driver.executeScript("mobile: launchApp", params);
+        driver.findElementByAccessibilityId("Open").click();
     }
 }
