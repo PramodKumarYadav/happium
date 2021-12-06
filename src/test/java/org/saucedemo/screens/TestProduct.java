1fd9f145df8863a973603161aa53a75493116442
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-12-06 21:46:44 +0100

    add tags to skip the ios test that I do not want to run for now (first would need a fix with scroll to element on ios)

--- src/test/java/org/saucedemo/screens/TestProduct.java
+++ src/test/java/org/saucedemo/screens/TestProduct.java
@@ -1,6 +1,7 @@
 package org.saucedemo.screens;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.saucedemo.deeplink.DeepLink.setDeepLinkUrl;
 
+@Tag("android")
 @ExtendWith(TestExecutionLifecycle.class)
 class TestProduct extends TestSetup {
     private ProductScreen productScreen;
