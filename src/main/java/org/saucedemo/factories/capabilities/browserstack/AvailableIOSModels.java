edad58953e4b0cbb2eb81368767eb36d16b4eca5
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-12-07 12:50:04 +0100

    only using iphone since ipad is not supported for our test app.

--- src/main/java/org/saucedemo/factories/capabilities/browserstack/AvailableIOSModels.java
+++ src/main/java/org/saucedemo/factories/capabilities/browserstack/AvailableIOSModels.java
@@ -4,8 +4,15 @@ import java.util.Random;
 
 // Available ios devices: https://www.browserstack.com/list-of-browsers-and-platforms/live
 public enum AvailableIOSModels {
-    IPHONE("iPhone"),
-    IPAD("iPad");
+    IPHONE("iPhone")
+    /**
+     * Reason for commenting out IPAD option: Our test app used in this project is only build for iphone.
+     * But if you real app is also supported on ipad then uncomment the below line to be used in fixed or random modes.
+     * Actual run log: [BROWSERSTACK_APP_BUILT_FOR_IPHONE] The device specified in the 'device' capability is ipad pro 11 2021.
+     * However, the app was built for iPhone. Please use an app built for iPad or specify an iPhone in 'device' capability
+    */
+//    , IPAD("iPad")
+    ;
 
     private final String value;
 
