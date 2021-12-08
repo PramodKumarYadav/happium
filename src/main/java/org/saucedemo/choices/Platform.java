992773a82c850a3988ea2d60efc2c50c6c8bf6c4
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-12-08 22:23:57 +0100

    enums are now all in upper case.

--- src/main/java/org/saucedemo/choices/Platform.java
+++ src/main/java/org/saucedemo/choices/Platform.java
@@ -1,6 +1,31 @@
 package org.saucedemo.choices;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Platform {
-    android,
-    ios
+    ANDROID("android"),
+    IOS("ios");
+
+    public final String label;
+    Platform(String label) {
+        this.label = label;
+    }
+
+    private static final Map<String, Platform> BY_LABEL = new HashMap<>();
+    static {
+        for (Platform platform: values()) {
+            BY_LABEL.put(platform.label, platform);
+        }
+    }
+
+    // To get enum name from a label
+    public static Platform valueOfLabel(String label) {
+        if(BY_LABEL.get(label) == null){
+            throw new IllegalStateException(String.format("%s is not a valid platform choice. Pick your platform from %s." +
+                    "Check the value of 'PLATFORM_NAME' property in application.conf; Or in CI, if running from continuous integration.", label, BY_LABEL.keySet()));
+        }else{
+            return BY_LABEL.get(label);
+        }
+    }
 }
--- src/main/java/org/saucedemo/factories/capabilities/browserstack/IOSModel.java
+++ src/main/java/org/saucedemo/factories/capabilities/browserstack/IOSModel.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Random;
 
 // Available ios devices: https://www.browserstack.com/list-of-browsers-and-platforms/live
-public enum AvailableIOSModels {
+public enum IOSModel {
     IPHONE("iPhone")
     /**
      * Reason for commenting out IPAD option: Our test app used in this project is only build for iphone.
@@ -17,19 +17,19 @@ public enum AvailableIOSModels {
     ;
 
     public final String label;
-    AvailableIOSModels(String label) {
+    IOSModel(String label) {
         this.label = label;
     }
 
-    private static final Map<String, AvailableIOSModels> BY_LABEL = new HashMap<>();
+    private static final Map<String, IOSModel> BY_LABEL = new HashMap<>();
     static {
-        for (AvailableIOSModels availableIOSModels: values()) {
-            BY_LABEL.put(availableIOSModels.label, availableIOSModels);
+        for (IOSModel IOSModel : values()) {
+            BY_LABEL.put(IOSModel.label, IOSModel);
         }
     }
 
     // To get enum name from a label
-    public static AvailableIOSModels valueOfLabel(String label) {
+    public static IOSModel valueOfLabel(String label) {
         if(BY_LABEL.get(label) == null){
             throw new IllegalStateException(String.format("%s is not a valid ios model. Pick your device from %s." +
                     "Check the value of 'DEVICE' property in browserstack.conf file; Or in CI -> if running from continuous integration.", label, BY_LABEL.keySet()));
@@ -38,7 +38,7 @@ public enum AvailableIOSModels {
         }
     }
 
-    public static AvailableIOSModels getRandomModel() {
+    public static IOSModel getRandomModel() {
         Random random = new Random();
         return values()[random.nextInt(values().length)];
     }
