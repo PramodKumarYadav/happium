4aa4fe443505c7317d911ea0d376cf9747d01522
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-12-08 22:23:57 +0100

    enums are now all in upper case.

--- src/main/java/org/saucedemo/choices/Host.java
+++ src/main/java/org/saucedemo/choices/Host.java
@@ -1,7 +1,31 @@
 package org.saucedemo.choices;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Host {
-    localhost,
-    browserstack,
-    saucelabs
+    LOCALHOST("localhost"),
+    BROWSERSTACK("browserstack"),
+    SAUCELABS("saucelabs");
+
+    public final String label;
+    Host(String label) {
+        this.label = label;
+    }
+
+    private static final Map<String, Host> BY_LABEL = new HashMap<>();
+    static {
+        for (Host host: values()) {
+            BY_LABEL.put(host.label, host);
+        }
+    }
+
+    // To get enum name from a label
+    public static Host valueOfLabel(String label) {
+        if(BY_LABEL.get(label) == null){
+            throw new IllegalStateException(String.format("%s is not a valid host choice. Pick your host from %s", label, BY_LABEL.keySet()));
+        }else{
+            return BY_LABEL.get(label);
+        }
+    }
 }
--- src/main/java/org/saucedemo/factories/capabilities/browserstack/AndroidModel.java
+++ src/main/java/org/saucedemo/factories/capabilities/browserstack/AndroidModel.java
@@ -1,13 +1,11 @@
 package org.saucedemo.factories.capabilities.browserstack;
 
-import org.saucedemo.choices.Platform;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 // Available android devices: https://www.browserstack.com/list-of-browsers-and-platforms/live
-public enum AvailableAndroidModels {
+public enum AndroidModel {
     SAMSUNG("samsung"),
     GOOGLE("google"),
     ONE_PLUS("one-plus"),
@@ -23,19 +21,19 @@ public enum AvailableAndroidModels {
     ;
 
     public final String label;
-    AvailableAndroidModels(String label) {
+    AndroidModel(String label) {
         this.label = label;
     }
 
-    private static final Map<String, AvailableAndroidModels> BY_LABEL = new HashMap<>();
+    private static final Map<String, AndroidModel> BY_LABEL = new HashMap<>();
     static {
-        for (AvailableAndroidModels availableAndroidModels: values()) {
-            BY_LABEL.put(availableAndroidModels.label, availableAndroidModels);
+        for (AndroidModel androidModel : values()) {
+            BY_LABEL.put(androidModel.label, androidModel);
         }
     }
 
     // To get enum name from a label
-    public static AvailableAndroidModels valueOfLabel(String label) {
+    public static AndroidModel valueOfLabel(String label) {
         if(BY_LABEL.get(label) == null){
             throw new IllegalStateException(String.format("%s is not a valid android model. Pick your device from %s." +
                     "Check the value of 'DEVICE' property in browserstack.conf file; Or in CI -> if running from continuous integration.", label, BY_LABEL.keySet()));
@@ -44,7 +42,7 @@ public enum AvailableAndroidModels {
         }
     }
 
-    public static AvailableAndroidModels getRandomModel() {
+    public static AndroidModel getRandomModel() {
         Random random = new Random();
         return values()[random.nextInt(values().length)];
     }
