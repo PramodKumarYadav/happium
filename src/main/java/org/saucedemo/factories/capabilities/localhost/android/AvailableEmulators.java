4bf46668c9b3e1fd7d9844f051f8ef08d2c057b1
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-12-09 18:35:35 +0100

    updated as per latest list of avds.

--- src/main/java/org/saucedemo/factories/capabilities/localhost/android/AvailableEmulators.java
+++ src/main/java/org/saucedemo/factories/capabilities/localhost/android/AvailableEmulators.java
@@ -1,12 +1,10 @@
 package org.saucedemo.factories.capabilities.localhost.android;
 
 public enum AvailableEmulators {
-    Pixel_3_API_31,
     Pixel_3a_API_29,
+    Pixel_3_API_31,
     Pixel_4_API_29,
     Pixel_4_API_30,
-    Pixel_4_API_31,
     Pixel_4a_API_31,
-    Pixel_5_API_31,
-    Pixel_XL_API_31
+    Pixel_5_API_31
 }
