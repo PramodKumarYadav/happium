9d8e5db78f2c40824183714e75ba8b0f0f3cc432
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2021-12-09 18:13:21 +0100

    update emulators list.

--- src/main/java/org/saucedemo/factories/capabilities/localhost/android/AvailableEmulators.java
+++ src/main/java/org/saucedemo/factories/capabilities/localhost/android/AvailableEmulators.java
@@ -1,11 +1,13 @@
 package org.saucedemo.factories.capabilities.localhost.android;
 
 public enum AvailableEmulators {
-    Pixel_4_API_31,
     Pixel_3_API_31,
     Pixel_3_XL_API_31,
+    Pixel_3a_API_29,
+    Pixel_4_API_29,
+    Pixel_4_API_30,
+    Pixel_4_API_31,
     Pixel_4a_API_31,
-    Pixel_4_XL_API_31,
     Pixel_5_API_31,
     Pixel_XL_API_31
 }
