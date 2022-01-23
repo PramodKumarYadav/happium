1662425ca4e866ab67aec2fe12e8d2922db0c84b
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2022-01-23 17:13:14 +0100

    remove references of AvailableEmulators Enum.

--- src/main/java/org/saucedemo/factories/capabilities/localhost/android/AvailableEmulators.java
+++
@@ -1,9 +0,0 @@
-package org.saucedemo.factories.capabilities.localhost.android;
-
-public enum AvailableEmulators {
-    Pixel_2_API_30,
-    Pixel_3_API_30,
-    Pixel_3a_API_29,
-    Pixel_4_API_29,
-    Pixel_4_API_30
-}
